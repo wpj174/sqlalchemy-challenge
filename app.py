0614fc81e8fba076fc1e7197963ff6316a439912
Bill Johnson <wpj174@gmail.com>
2021-11-21 19:34:32 -0500

    Update app.py
    Precip API works!

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-# 1. import Flask
-from flask import Flask
+#################################################
+# Import libraries
+#################################################
 
-# 2. Create an app, being sure to pass __name__
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
 
@@ -11,22 +38,52 @@ app = Flask(__name__)
 def home():
     print("Server received request for 'Home' page...")
     return (
-        f"Welcome to the Wether Observation Home page!<br/>"
+        f"<h1>Welcome to the Weather Observation Home page!</h1><br/>"
         f"<br/>"
-        f"Available Routes:<br/>"
-        f"/ - This page<br/>"
-        f"/api/v1.0/precipitation - Rainfall observations<br/>"
-        f"/api/v1.0/stations - Weather observation stations<br/>"
-        f"/api/v1.0/tobs - Temperature observations from the most active station for the past year<br/>"
-        f"/api/v1.0/<start>[/<end>] - Returns minimum, average and maximum temperatures for the date range (yyyy-mm-dd)<br/>"
+        f"<h2>Available Routes:</h2><br/>"
+        f"<table border=1>"
+        f"<tr>"
+        f"<th style='padding: 10px'>Route</th><th style='padding: 10px'>Returns</th>"
+        f"</tr>"
+        f"<tr>"
+        f"<td style='padding: 10px'>/</td><td style='padding: 10px'>This page</td>"
+        f"</tr>"
+        f"<tr>"
+        f"<td style='padding: 10px'>/api/v1.0/precipitation</td><td style='padding: 10px'>Rainfall observations</td>"
+        f"</tr>"
+        f"<tr>"
+        f"<td style='padding: 10px'>/api/v1.0/stations</td><td style='padding: 10px'>Weather observation stations</td>"
+        f"</tr>"
+        f"<tr>"
+        f"<td style='padding: 10px'>/api/v1.0/tobs</td><td style='padding: 10px'>Temperature observations from the most active station for the past year</td>"
+        f"</tr>"
+        f"<tr>"
+        f"<td style='padding: 10px'>/api/v1.0/<start>[/<end>]</td><td style='padding: 10px'>Returns minimum, average and maximum temperatures for the date range (yyyy-mm-dd)</td>"
+        f"</tr>"
+        f"</table>"
     )
 
 
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
+# Precip route
+# - report rain data
+@app.route("/api/v1.0/precipitation")
 def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all rainfall observations"""
+    # Query all rainfall measurements
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into dict
+    all_precip = []
+    
+    for result in results:
+        all_precip.append(dict(result))
+
+    return jsonify(all_precip)
 
 
 if __name__ == "__main__":
